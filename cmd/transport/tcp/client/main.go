8d215a4243342c9f810552704b9619687d6697e5
brewlin <97404667@qq.com>
2019-11-09 23:35:37 +0800

    client demo & fix read()

--- cmd/transport/tcp/client/main.go
+++ cmd/transport/tcp/client/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/brewlin/net-protocol/pkg/logging"
 	"github.com/brewlin/net-protocol/protocol/transport/tcp/client"
 	_ "github.com/brewlin/net-protocol/stack/stackinit"
-	"fmt"
 )
 
 func init() {
@@ -14,6 +15,6 @@ func main() {
 	con := client.NewClient("127.0.0.1", 8080)
 	con.Connect()
 	con.Write([]byte("send msg"))
-	res := con.Read()
+	res, _ := con.Read()
 	fmt.Println(res)
 }

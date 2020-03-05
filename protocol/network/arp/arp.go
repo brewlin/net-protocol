ed273461756aa88232462cc6955bc73ca19d6b23
brewlin <97404667@qq.com>
2020-03-05 14:23:30 +0800

    Fix #2

--- protocol/network/arp/arp.go
+++ protocol/network/arp/arp.go
@@ -28,9 +28,9 @@ package arp
 import (
 	"log"
 
-	"github.com/brewlin/net-protocol/stack"
 	"github.com/brewlin/net-protocol/pkg/buffer"
 	"github.com/brewlin/net-protocol/protocol/header"
+	"github.com/brewlin/net-protocol/stack"
 
 	tcpip "github.com/brewlin/net-protocol/protocol"
 )
@@ -110,9 +110,10 @@ func (e *endpoint) HandlePacket(r *stack.Route, vv buffer.VectorisedView) {
 		pkt.SetIpv4OverEthernet()
 		pkt.SetOp(header.ARPReply)
 		copy(pkt.HardwareAddressSender(), r.LocalLinkAddress[:])
+		copy(pkt.HardwareAddressTarget(),h.HardwareAddressSender())
 		copy(pkt.ProtocolAddressSender(), h.ProtocolAddressTarget())
 		copy(pkt.ProtocolAddressTarget(), h.ProtocolAddressSender())
-		log.Println("@网络层 arp: reply: 发送arp回复")
+		log.Println("@网络层 arp: reply: 发送arp回复",h.ProtocolAddressSender(),h.HardwareAddressTarget())
 		e.linkEP.WritePacket(r, hdr, buffer.VectorisedView{}, ProtocolNumber)
 		// 注意这里的 fallthrough 表示需要继续执行下面分支的代码
 		// 当收到 arp 请求需要添加到链路地址缓存中
